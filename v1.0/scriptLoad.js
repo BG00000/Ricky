94b379d2bbb23ce8ce2852c8f5ece0b9aa17df96
BG00000 <abakergabb@gmail.com>
2024-02-24 10:57:44 +1100

    loading_animation

---
+++ v1.0/scriptLoad.js
@@ -0,0 +1,44 @@
+// window.onload = () => { //loading animation
+
+//     const loadingAnimation = document.getElementById('loading-animation')
+//     const loadingCover = document.getElementById('loading-cover')
+
+//     let array = []
+//     let loadWord = 'Ricky.'
+
+//     for(let i = 0; i < loadWord.length; i++) {
+//         array.push(loadWord[i])
+//     }
+
+//     function animateTyping () {
+//         array.forEach((element, index) => {
+//             let interval = 100
+//             setTimeout(() => {
+//                 loadingAnimation.innerText += element
+//             }, index * interval)
+//         })
+//     }
+
+//     setTimeout(animateTyping, 80)
+
+//     function animateJump() {
+//         loadingAnimation.style.transform = "translateY(-200px)"
+//     }
+
+//     setTimeout(animateJump, 400)
+
+//     function finishAnimation() {
+//         loadingAnimation.style.transform = "translateY(-400px)"
+//         loadingAnimation.style.opacity = '0' 
+//         loadingCover.style.opacity = '0'
+//     }
+
+//     setTimeout(finishAnimation, 1000)
+
+//     function removeCover () {
+//         loadingCover.style.display = 'none'
+//     }
+
+//     setTimeout(removeCover, 1300)
+// }
+
