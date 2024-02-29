0837f3c14618c6a24c8e703ab8c74441c268fefd
BG00000 <abakergabb@gmail.com>
2024-03-01 09:16:12 +1100

    wawa

--- v1.0/scriptMain.js
+++ v1.0/scriptMain.js
@@ -1,26 +1,55 @@
 
-const targetVW = [900, 800, 700, 600, 500, 400]
-const exploreButtonNames = ['About Ricky', 'Naughty & Nice', 'Fashion', 'Vitals', 'Likes & Dislikes', 'Gallery', 'Interview', 'Pedigree', 'Friends']
+//I generate the buttons here since I struggled to make them responsive to screen size changes in the way I wanted with them in html
 
-const exploreButtonColors = [
-    '#b8ff7d',
-    '#b8ff7d',
-    '#b8ff7d',
-    '#7dffc2',
-    '#b8ff7d',
-    '#b8ff7d',
-    '#b8ff7d',
-    '#b8ff7d',
-    '#7dffc2'
-]
 
-for (let i = 0; i <= 8; i++) {
-    const exploreBarBottom = document.getElementById('explore-bar-bottom')
 
-    const exploreBarButtons = document.createElement('button')
-    exploreBarButtons.id = i
-    exploreBarButtons.innerText = exploreButtonNames[i]
-    exploreBarButtons.style.backgroundColor = exploreButtonColors[i]
-    exploreBarBottom.appendChild(exploreBarButtons)
+function resizeFn() {
+
+    const targetVW = [900, 800, 700, 600, 500, 400]
+
+    let j = 0
+
+    const exploreButtonNames = ['About Ricky', 'Naughty & Nice', 'Fashion', 'Vitals', 'Likes & Dislikes', 'Gallery', 'Interview', 'Pedigree', 'Friends']
+
+    const exploreButtonColors = [
+        '#b8ff7d',
+        '#b8ff7d',
+        '#b8ff7d',
+        '#7dffc2',
+        '#b8ff7d',
+        '#b8ff7d',
+        '#b8ff7d',
+        '#b8ff7d',
+        '#7dffc2'
+    ]
+    
+    for (let i = 0; i <= 8; i++) {
+
+        const exploreBarBottom = document.getElementById('explore-bar-bottom')
+    
+        const exploreBarButtons = document.createElement('button')
+        exploreBarButtons.id = i
+        exploreBarButtons.innerText = exploreButtonNames[i]
+        exploreBarButtons.style.backgroundColor = exploreButtonColors[i]
+        exploreBarBottom.appendChild(exploreBarButtons)
+    
+        if (window.innerWidth <= targetVW[j]) {
+            
+        }
+
+        .style.backgroundColor = 
+    }
+
 }
+
+window.addEventListener('resize', resizeFn, true)
+
+resizeFn()
+
+
+
+
+
+
+
 
