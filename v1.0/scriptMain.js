4909761b11b3271b3d1e1f46d174ad889785f5e8
BG00000 <abakergabb@gmail.com>
2024-03-02 03:05:29 +1100

    major refactoring and redesigning

--- v1.0/scriptMain.js
+++ v1.0/scriptMain.js
@@ -1,21 +1,59 @@
 
 // I just wrote this to make my CSS look more clean ;)
 
-const exploreButtonColors = [
-    '#ff593f',
-    '#ff753f',
-    '#ff9f3f',
-    '#B3D885',
-    '#88D798',
-    '#5DD5AB',
-    '#3CDBD3',
-    '#1EEDE4',
-    '#00FFF5'
-]
+// function handleButtonColors () {
+//     const exploreButtonColors = [
+//         '#ff593f',
+//         '#ff753f',
+//         '#ff9f3f',
+//         '#B3D885',
+//         '#88D798',
+//         '#5DD5AB',
+//         '#3CDBD3',
+//         '#1EEDE4',
+//         '#00FFF5'
+//     ]
+    
+//     const exploreBarBottom = document.getElementById('explore-bar-bottom')
+//     const exploreBarButtons = exploreBarBottom.querySelectorAll('button')
+    
+//     exploreBarButtons.forEach((element, index) => {
+//         element.style.backgroundColor = exploreButtonColors[index]
+//     })
+// }
 
-const exploreBarBottom = document.getElementById('explore-bar-bottom')
-const exploreBarButtons = exploreBarBottom.querySelectorAll('button')
+// handleButtonColors()
 
-exploreBarButtons.forEach((element, index) => {
-    element.style.backgroundColor = exploreButtonColors[index]
-})
+function handleExploreExpand () {
+    const exploreExpand = document.querySelector('.explore-expand')
+    const expandAboutRicky = document.getElementById('expand-about-ricky')
+
+    //buttons (hover)
+
+    const aboutRickyBtn = document.getElementById('about-ricky-btn')
+
+
+    aboutRickyBtn.addEventListener('mouseover', () => {
+        exploreExpand.style.display = 'flex'
+        expandAboutRicky.style.display = 'flex'
+    })
+
+    exploreExpand.addEventListener('mouseover', () => {
+        exploreExpand.style.display = 'flex'
+        expandAboutRicky.style.display = 'flex'
+    })
+
+    aboutRickyBtn.addEventListener('mouseout', () => {
+        exploreExpand.style.display = 'none'
+        expandAboutRicky.style.display = 'none'
+    })
+
+    exploreExpand.addEventListener('mouseout', () => {
+        exploreExpand.style.display = 'none'
+        expandAboutRicky.style.display = 'none'
+    })
+
+
+}
+
+handleExploreExpand()
