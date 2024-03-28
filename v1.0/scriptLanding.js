d58da276a171508e5d57f9b8968ec5abd1bc9d9b
BG00000 <abakergabb@gmail.com>
2024-03-28 11:11:19 +1100

    fleshing out landing page, CSS animations

--- v1.0/scriptLanding.js
+++ v1.0/scriptLanding.js
@@ -4,18 +4,20 @@ function handleScrollPrompt () {
     window.addEventListener('scroll', () => {
 
         scrollInfo = window.scrollY / 1000
+        arrowOpacity = 1 - 3 * (scrollInfo)
 
         if(scrollInfo === 0) {
             scrollPrompt.style.opacity = 1
         } else if(scrollInfo > 0) {
-            scrollPrompt.style.opacity = `${1 - 1.5 * (scrollInfo)}`
+            scrollPrompt.style.opacity = `${arrowOpacity}`
         }
 
         //I don't want it to run in the background if the user scrolls far
 
-        if(scrollInfo > 1200) {
+        if(arrowOpacity < 0) {
             scrollPrompt.style.display = "none"
-        } else if(scrollInfo < 1200) {
+            scrollPrompt.style.opacity = 0
+        } else if(arrowOpacity > 0) {
             scrollPrompt.style.display = "inline"
         }
     })
