63e5c4fd7d9beab27cdc33475b0a56291857405d
u7436429 <u7436429@anu.edu.au>
2024-05-16 15:43:28 +1000

    FFFFFF

--- v1.0/scriptMain.js
+++ v1.0/scriptMain.js
@@ -36,49 +36,26 @@ function handleExploreExpand() {
 }
    
 handleExploreExpand()
- 
-function handleExplorePill() {
-    const explorePill = document.getElementById('explore-pill')
-    explorePill.addEventListener('mouseover', () => {
-       explorePill.innerText = 'Still under construction, sorry!'
-    })
-    explorePill.addEventListener('mouseout', () => {
-       explorePill.innerText = 'Explore'
-    })
-}
-
-handleExplorePill()
 
 
 function handleThemes() {
 
-   
-   localStorage.setItem("theme", "dark")
    const themeInput = document.querySelector(".theme-input")
    const html = document.querySelector("html")
 
-
-
-   // Check the browser preferred color scheme, and sets the defaultTheme based of that
    const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
    const defaultTheme = prefersDarkMode ? "dark" : "light";
    const preferredTheme = localStorage.getItem("theme")
 
-   // Check if the localStorage item is set, if not set it to the default theme
    if (!preferredTheme) {
       localStorage.setItem("theme", defaultTheme);
    }
 
    html.dataset.theme = preferredTheme || defaultTheme;
 
-
-   // Theme toggle handler
    themeInput.addEventListener("click", () => {
-      // Check if the saved theme in localStorage is  "dark"
       const isDarkTheme = localStorage.getItem("theme") === "dark";
-      // Chooses the opposite theme of the current selected one
       const newTheme = isDarkTheme ? "light" : "dark"
-      // Changes the theme to the newTheme
       localStorage.setItem("theme", newTheme);
       html.dataset.theme = newTheme;
       console.log(localStorage.getItem("theme"))
@@ -87,21 +64,22 @@ function handleThemes() {
 
 handleThemes()
 
-//cookies - JSON - 
 
 function handleCSSFiles() {
-   const landingCSSLink = document.getElementById("landingCSSLink")
+
    const printBtn = document.getElementById("print-btn")
+
+   const landingCSSLink = document.getElementById("landingCSSLink")
 
    printBtn.addEventListener("click", () => {
       if (landingCSSLink.getAttribute("href") === "styleLanding.css") {
-         landingCSSLink.setAttribute("href", "styleLandingAlt.css")
+         landingCSSLink.setAttribute("href", "styleAlt.css")
       } else {
          landingCSSLink.setAttribute("href", "styleLanding.css")
       }
    })
 }
 
-handleCSSFiles()
+// handleCSSFiles()
 
 
